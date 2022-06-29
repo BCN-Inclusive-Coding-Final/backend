01b980d332a83c6fd721cbe0dd75084e4434714c
Daniel Españadero <developer@daniel-espanadero.com>
2022-06-29 20:40:00 +0200

    SignUp started

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,4 +1,35 @@
+import { User } from "../models/User";
+
+export const SignUp = async (req, res) => {
+    try {
+        const { firstName, lastName, email, password } = req.body;
+
+        //  Create user
+        const user = await new User({
+            firstName,
+            lastName,
+            email,
+            password: await User.encryptPassword(password)
+        });
+
+        // Save to DB
+        const saveUser = await user.save();
+
+        // Generate JWT
+
+        res.status(200).json({
+            user
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            msg: 'Error 500 - Internal Server Error'
+        })
+    };
+};
+
 export const signIn = (req, res) => {
+
     try {
         res.status(200).json({
             msg: 'Login'
@@ -8,8 +39,4 @@ export const signIn = (req, res) => {
             msg: 'Error 500 - Internal Server Error'
         });
     };
-};
-
-export const SignUp = (req, res) => {
-
 };
