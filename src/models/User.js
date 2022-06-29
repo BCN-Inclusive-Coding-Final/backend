26b0abf03ca128331c9f2018b5a784687a1ec0fb
Daniel Españadero <developer@daniel-espanadero.com>
2022-06-29 20:23:07 +0200

    UserSchema and model created

---
+++ src/models/User.js
@@ -0,0 +1,34 @@
+import { Schema, model } from "mongoose";
+import bcryptjs from 'bcryptjs'
+
+const UserSchema = new Schema({
+    firstName: {
+        type: String,
+        required: [true, 'First name is required!!']
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Last name is required!!']
+    },
+    email: {
+        type: String,
+        unique: true,
+        match: [/.+\@.+\..+/, '** Email is invalid **'],
+        required: [true, 'Email is required!!']
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required!!']
+    }
+});
+
+UserSchema.statics.encryptPassword = async () => {
+    const salt = await bcryptjs.genSalt(10);
+    return await bcryptjs.hash(password, salt)
+};
+
+UserSchema.statics.comparePassword = async (password, receivedPassword) => {
+    return await bcryptjs.compare(password, receivedPassword);
+};
+
+export const User = model('User', UserSchema);
