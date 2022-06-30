d18e907c5772ba56d0e6b1a90f4281289abc7348
Daniel Españadero <developer@daniel-espanadero.com>
2022-06-30 12:36:16 +0200

    Update model post

--- src/controllers/posts.controllers.js
+++ src/controllers/posts.controllers.js
@@ -1,11 +1,17 @@
 import { Post } from '../models/Post'
 
 export const createPost = async (req, res) => {
-  const post = req.body
-  const newPost = new Post({ ...post, creator: req.userId, createdAt: new Date().toISOString() })
+  const {title, category, style, description, image} = req.body
+  const newPost = new Post({
+    title,
+    category,
+    style,
+    description,
+    image
+  })
   try {
     await newPost.save()
-    res.status(201).json(newPost)
+    res.status(201).json({newPost})
   } catch (error) {
     res.status(409).json({ message: error.message })
   }
--- src/models/Post.js
+++ src/models/Post.js
@@ -3,22 +3,25 @@ import { Schema, model } from "mongoose"
 const PostSchema = new Schema({
     title: {
         type: String,
-        require: [true, 'Title is requied']
+        required: [true, 'Title is requied']
     },
     category: {
         type: String,
-        require: [true, 'Category is requied']
+        required: [true, 'Category is requied']
     },
     state: String,
     description: {
         type: String,
-        require: [true, 'Description is requied']
+        required: [true, 'Description is requied']
     }, 
     image: {
         type: String,
         default: 'image/url'
     },
     createdAt: { type: Date, default: new Date() }
-})
+},
+    {
+        versionKey: false
+    })
 
 export const Post = model('Post', PostSchema);
