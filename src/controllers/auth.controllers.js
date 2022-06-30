6ede381db06ca4e2ef3fc22324113a6725a7a724
Juan Omen <anvidmen@gmail.com>
2022-06-30 16:24:00 +0200

    update user controller

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -2,8 +2,8 @@ import { User } from '../models/User';
 import { sign } from 'jsonwebtoken';
 
 export const SignUp = async (req, res) => {
+    const { firstName, lastName, email, password } = req.body;
     try {
-        const { firstName, lastName, email, password } = req.body;
 
         //  Create user
         const user = await new User({
@@ -14,25 +14,24 @@ export const SignUp = async (req, res) => {
         });
 
         // Check if email exists
-        const emailExist = await User.find({email})
-        if(emailExist){
-            return res.status(400).json({
-                msg: 'That email already exists'
-            });
-        };
+        const emailExist = await User.findOne({ email })
+        if (emailExist) return res.status(400).json({ msg: 'That email already exists' })
+
 
         // Save to DB
-        const saveUser = await user.save();
+        const saveUser = await User.create({email, password, firstName, lastName});
 
         // Generate JWT
         const token = sign({ id: saveUser._id }, process.env.SECRETORPRIVATEKEY, { expiresIn: '4h' })
-
+        console.log("token",token)
         res.status(200).json({
             user,
             token
         })
 
     } catch (error) {
+
+        console.log("error", error)
         res.status(500).json({
             msg: 'Error 500 - Internal Server Error',
         })
@@ -54,7 +53,7 @@ export const signIn = async (req, res) => {
         // Validate password
         const validPassword = await User.comparePassword(req.body.password, userDB.password);
 
-        if(!validPassword){
+        if (!validPassword) {
             return res.status(403).json({
                 token: null,
                 msg: 'The password you entered is not correct.'
